cf7648fa568b3be6f776c469c7d5387df4db10a0
airbowen <airbowen@163.com>
2024-01-22 17:29:29 +0800

    hardhat  commit

---
+++ scripts/deploy.js
@@ -0,0 +1,57 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+
+// 这个是部署 lock 的deploy
+// const hre = require("hardhat");
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.parseEther("0.001");
+
+//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   await lock.waitForDeployment();
+
+//   console.log(
+//     `Lock with ${ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+// 这里才是部署counter 的deploy
+const { ethers } = require("hardhat");
+
+async function main() {
+
+     const Counter = await ethers.getContractFactory("Counter");
+   const counter = await Counter.deploy();
+   await counter.deployed();
+
+  console.log("Counter address:", counter.address);
+}
+
+// async function main() {
+//   const Counter = await ethers.getContractFactory("Counter");
+//   const counter = await Counter.deploy();
+//   await counter.deployTransaction.wait(); // wait for the transaction to be mined
+//   console.log("Counter address:", counter.address);
+// }
+
+main();
+
